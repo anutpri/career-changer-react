c8eed709f4d61263dfda78c2968542b9e47579e8
anutpri <anut.pri@gmail.com>
2023-04-10 15:45:06 +0700

    finish ex07

--- 04-react-function-and-hook/02-state-hook-usestate/07-exercise/src/App.jsx
+++ 04-react-function-and-hook/02-state-hook-usestate/07-exercise/src/App.jsx
@@ -2,39 +2,49 @@ import React, { useState } from 'react';
 
 function App() {
     // adding state here.
+const [choices, setChoices] = useState()
 
-  const handleClick = (value) => {
-    // code here.
-  };
+  // const handleClick = (value) => {
+  //   // code here.
+  //   setChoices(value);
+  // };
 
   return (
+    // <div>
+    //   <button onClick={() => handleClick('Fullname')}>Fullname</button>
+    //   <button onClick={() => handleClick('Age')}>Age</button>
+    //   <button onClick={() => handleClick('Picture')}>Picture</button>
+    //   <DisplayInfo choicesValue={choices}/>
+    // </div>
     <div>
-      <button>Fullname</button>
-      <button>Age</button>
-      <button>Picture</button>
-      <DisplayInfo />
+      <button onClick={() => setChoices('Fullname')}>Fullname</button>
+      <button onClick={() => setChoices('Age')}>Age</button>
+      <button onClick={() => setChoices('Picture')}>Picture</button>
+      <DisplayInfo choicesValue={choices}/>
     </div>
   );
 }
 
 function DisplayInfo(props) {
 
-  let ___;
-  if (___ === 'Fullname') {
-    ___ = <h2>John Doe</h2>;
-  } else if (___ === 'Age') {
-    ___ = <h2>30</h2>;
-  } else if (___ === 'Picture') {
-    ___ = <img src="https://via.placeholder.com/150" alt="Placeholder" />;
+  
+ 
+  let content ='';
+  if (props.choicesValue === 'Fullname') {
+    content = <h2>John Doe</h2>;
+  } else if (props.choicesValue === 'Age') {
+    content = <h2>30</h2>;
+  } else if (props.choicesValue === 'Picture') {
+    content = <img width={200} src="https://rnz-ressh.cloudinary.com/image/upload/s--7yAMXCvu--/ar_16:10,c_fill,f_auto,g_auto,q_auto,w_1050/v1656446001/4LPFVF5_cat_nip_jpg" alt="Placeholder" />;
   } else {
-    ___ = <p>Please select an option.</p>;
+    content = <p>Please select an option.</p>;
   }
 
   return (
     <div>
-      {___}
+      {content}
     </div>
-  );
+  )
 }
 
 export default App;
