244e9fdad0ba75b04d47a42a7265f10f8ccf55e5
anutpri <anut.pri@gmail.com>
2023-05-02 14:50:03 +0700

    finish ex04

--- 08-nodejs/04-exercise/src/main.js
+++ 08-nodejs/04-exercise/src/main.js
@@ -6,7 +6,34 @@ const ipAddress = "127.0.0.1";
 const port = 3001;
 
 // code here.
-const company = [];
+const company = [{
+  companyId: "comp-01",
+  name: "Mock Company 01",
+  taxId: "tax-000",
+  employees: []
+}];
+
+webServer.get("/", (request, response) => {
+  response.send("Hello World");
+});
+
+webServer.get("/company", (request, response) => {
+  
+
+  response.json(company);
+});
+
+webServer.post("/company", (request, response) => {
+  const newData  = { 
+    id: company.length + 1,
+    name : request.body.name, 
+    taxId : request.body.taxId,
+    employees: [],
+  };
+
+  company.push(newData);
+  response.send("Create User Success");
+});
 
 webServer.listen(port, ipAddress, () => {
   console.log(`Web Application Server is running on ${ipAddress} port ${port}`);
